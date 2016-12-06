19584a312c092b8d27d19f89e2cc9f1f489d00b5
Dominique Kreemers <dominique.kreemers@me.com>
2016-12-06 12:54:21 +0100

    [TASK] Remove license annotation from viewHelper class

--- Classes/ViewHelpers/DefinitionAnchorViewHelper.php
+++ Classes/ViewHelpers/DefinitionAnchorViewHelper.php
@@ -30,9 +30,6 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
  * IndexViewHelper
- *
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
- *
  */
 class DefinitionAnchorViewHelper extends AbstractViewHelper
 {
