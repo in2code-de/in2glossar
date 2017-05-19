83b4782637024c5d02f2175a0e537f719cb2b614
Marcus Schwemer <marcus.schwemer@in2code.de>
2017-05-19 15:21:58 +0200

    [TASK] Release preparations
    * Raise version number
    
    https://projekte.in2code.de/issues/26748

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,17 +6,17 @@
 
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'In2glossar',
-    'description' => 'Provides a glossar list for TYPO3',
+    'description' => 'Provides a glossary for TYPO3',
     'category' => 'misc',
     'author' => 'in2code GmbH',
     'author_email' => 'service@in2code.de',
     'dependencies' => 'extbase, fluid',
     'state' => 'stable',
     'author_company' => 'in2code GmbH',
-    'version' => '0.1.0',
+    'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.5.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
