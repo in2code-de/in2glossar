614d8edbd544021d2371437747590a70c61ded6e
Alexander Kellner <alexander.kellner@in2code.de>
2021-11-18 14:50:08 +0100

    [BUGFIX] Backend list view should show items sorted by word

--- Configuration/TCA/tx_in2glossar_domain_model_definition.php
+++ Configuration/TCA/tx_in2glossar_domain_model_definition.php
@@ -8,6 +8,7 @@ return [
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
         'cruser_id' => 'cruser_id',
+        'sortby' => 'word',
         'dividers2tabs' => true,
         'languageField' => 'sys_language_uid',
         'transOrigPointerField' => 'l10n_parent',
