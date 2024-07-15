3d3da4fb54bff215c085bf9d6250ad8514c88ae5
Oliver Eglseder <oliver.eglseder@in2code.de>
2024-07-15 14:43:13 +0200

    [CLEANUP] Remove fields managed by TYPO3 from ext_tables.sql
    Resolves: https://projekte.in2code.de/issues/65260

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,28 +1,9 @@
 CREATE TABLE tx_in2glossar_domain_model_definition (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	word varchar(255) DEFAULT '' NOT NULL,
 	synonyms tinytext NOT NULL,
 	short_description tinytext NOT NULL,
 	description text NOT NULL,
-	tooltip tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY language (l10n_parent,sys_language_uid)
+	tooltip tinyint(4) unsigned DEFAULT '0' NOT NULL
 );
 
 CREATE TABLE tt_content (
