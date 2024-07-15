8ef1c140dc42a75bacc064fc54d60e570f994427
Oliver Eglseder <oliver.eglseder@in2code.de>
2024-07-15 15:00:14 +0200

    [REFACTOR] Import namespaces and remove the userfunc safeguard
    ext_*.php files are wrapped in namespace blocks, imports can not escape and variables are safe
    
    Resolves: https://projekte.in2code.de/issues/65260

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,39 +1,40 @@
 <?php
+
+use In2code\In2glossar\Controller\StandardController;
+use In2code\In2glossar\Hooks\ContentPostProcessor;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+use TYPO3\CMS\Core\Imaging\IconRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(
-    function () {
-
-        /**
-         * Register Icons
-         */
-        $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-            \TYPO3\CMS\Core\Imaging\IconRegistry::class
-        );
-        $iconRegistry->registerIcon(
-            'extension-in2glossar-definition',
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => 'EXT:in2glossar/Resources/Public/Icons/definition.svg']
-        );
-
-        /**
-         * Include Frontend Plugins
-         */
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'In2glossar',
-            'Main',
-            [
-                \In2code\In2glossar\Controller\StandardController::class => 'list',
-            ],
-            []
-        );
-
-        /**
-         * Add tooltips to rendered frontend
-         */
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][]
-            = \In2code\In2glossar\Hooks\ContentPostProcessor::class . '->render';
-    }
+/**
+ * Register Icons
+ */
+$iconRegistry = GeneralUtility::makeInstance(
+    IconRegistry::class,
 );
+$iconRegistry->registerIcon(
+    'extension-in2glossar-definition',
+    SvgIconProvider::class,
+    ['source' => 'EXT:in2glossar/Resources/Public/Icons/definition.svg'],
+);
+
+/**
+ * Include Frontend Plugins
+ */
+ExtensionUtility::configurePlugin(
+    'In2glossar',
+    'Main',
+    [
+        StandardController::class => 'list',
+    ],
+);
+
+/**
+ * Add tooltips to rendered frontend
+ */
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = ContentPostProcessor::class . '->render';
