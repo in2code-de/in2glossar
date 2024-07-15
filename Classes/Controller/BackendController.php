9adf35dd3325978dacb9c5d6c92beeec5da25efa
Oliver Eglseder <oliver.eglseder@in2code.de>
2024-07-15 14:05:19 +0200

    [REFACTOR] Reduce class hierarchy of the BackendController
    Resolves: https://projekte.in2code.de/issues/65260

--- Classes/Controller/BackendController.php
+++ Classes/Controller/BackendController.php
@@ -3,11 +3,16 @@ declare(strict_types=1);
 namespace In2code\In2glossar\Controller;
 
 use TYPO3\CMS\Core\Messaging\AbstractMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
  * Class BackendController
  */
-class BackendController extends AbstractBackendController
+class BackendController extends ActionController
 {
     /**
      * @return void
@@ -24,5 +29,42 @@ class BackendController extends AbstractBackendController
      */
     public function indexAction()
     {
+    }
+
+    /**
+     * @param string $messageBody
+     * @param string $messageTitle
+     * @param int $severity
+     * @param bool $storeInSession
+     */
+    public function addFlashMessage(
+        $messageBody,
+        $messageTitle = '',
+        $severity = AbstractMessage::OK,
+        $storeInSession = true
+    ) {
+        /** @var FlashMessage $message */
+        $message = GeneralUtility::makeInstance(
+            FlashMessage::class,
+            $messageBody,
+            $messageTitle,
+            $severity,
+            $storeInSession
+        );
+        /* @var $flashMessageService FlashMessageService */
+        $flashMessageService = GeneralUtility::makeInstance(
+            FlashMessageService::class
+        );
+        $messageQueue = $flashMessageService->getMessageQueueByIdentifier();
+        $messageQueue->addMessage($message);
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isStoragePidGiven(): bool
+    {
+        return MathUtility::canBeInterpretedAsInteger($this->settings['storagePid'])
+            && $this->settings['storagePid'] > 0;
     }
 }
